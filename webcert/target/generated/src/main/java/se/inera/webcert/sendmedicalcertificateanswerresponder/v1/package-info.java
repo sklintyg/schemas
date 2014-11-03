a982c87ded21e6f58887a495bd34fa9bdb58acf4
Niklas Pettersson <niklas.pettersson@r2m.se>
2014-11-03 14:36:34 +0100

    Tagit bort genererade filer som inte ska versionshanteras

--- webcert/target/generated/src/main/java/se/inera/webcert/sendmedicalcertificateanswerresponder/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:riv:insuranceprocess:healthreporting:SendMedicalCertificateAnswerResponder:1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package se.inera.webcert.sendmedicalcertificateanswerresponder.v1;
--- webcert/target/generated/src/main/java/se/riv/ehr/log/store/v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:riv:ehr:log:store:1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package se.riv.ehr.log.store.v1;
