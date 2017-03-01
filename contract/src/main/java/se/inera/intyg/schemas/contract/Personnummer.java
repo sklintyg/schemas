46fba4c9680212349c7474d2a6122db93438bb24
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-03-01 13:08:39 +0100

    INTYG-3150: Nytt projekt under schemas för att hålla vår globala Personnummer-klass

---
+++ contract/src/main/java/se/inera/intyg/schemas/contract/Personnummer.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2017 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.intyg.schemas.contract;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.google.common.base.Objects;
+import se.inera.intyg.schemas.contract.util.HashUtility;
+
+import java.util.Calendar;
+import java.util.Optional;
+
+
+@JsonDeserialize(using = PersonnummerDeserializer.class)
+public class Personnummer {
+
+    private final String pnr;
+
+    public Personnummer(String pnr) {
+        this.pnr = pnr;
+    }
+
+    /**
+     * Returns a Personnummer with a dash, iff the String is a valid personnummer.
+     * @return Validated personnummer on form (19|20)[0-9]{6}-[0-9]{4}
+     */
+    public static Optional<Personnummer> createValidatedPersonnummerWithDash(String nonValidatedPnr) {
+        try {
+            String pnrWithoutDash = new Personnummer(nonValidatedPnr).getNormalizedPnr();
+            //CHECKSTYLE:OFF MagicNumber
+            return Optional.of(new Personnummer(pnrWithoutDash.substring(0, 8) + '-' + pnrWithoutDash.substring(8, 12)));
+            //CHECKSTYLE:ON MagicNumber
+        } catch (InvalidPersonNummerException e) {
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<Personnummer> createValidatedPersonnummerWithDash(Personnummer nonValidatedPnr) {
+        return createValidatedPersonnummerWithDash(nonValidatedPnr.getPersonnummer());
+    }
+
+    @JsonValue
+    public String getPersonnummer() {
+        return pnr;
+    }
+
+    /**
+     * Will return the hashed personnummer to make sure the real personnummer is not accidentally logged.
+     * @return Hashed personnummer
+     */
+    @Override
+    public String toString() {
+        assert false : "Don't use implicit toString. Use explicit getPnrHash or getPersonnummer instead.";
+        return getPnrHash();
+    }
+
+    public String getPnrHash() {
+        return HashUtility.hash(getNormalizedPnrIfPossible(pnr));
+    }
+
+    /**
+     * Returns the normalized personnummer if it is valid, otherwise returns the originally set personnummer.
+     */
+    private String getNormalizedPnrIfPossible(String returnValueWhenInvalid) {
+        try {
+            return getNormalizedPnr();
+        } catch (InvalidPersonNummerException e) {
+            return returnValueWhenInvalid;
+        }
+    }
+
+    /**
+     * Get the personnummer in a standardized format (yyyyMMddxxxx) regardless of how it was entered.
+     */
+    public String getNormalizedPnr() throws InvalidPersonNummerException {
+        if (pnr == null) {
+            throw new InvalidPersonNummerException("Can not normalize null");
+        }
+        if (pnr.matches("^(19|20)[0-9]{6}[-+]?[0-9]{4}$")) {
+            return pnr.replace("-", "").replace("+", "");
+        }
+        if (pnr.matches("^[0-9]{6}[+-]?[0-9]{4}$")) {
+            return getCenturyFromYearAndSeparator(pnr) + pnr.replace("-", "").replace("+", "");
+        }
+        throw new InvalidPersonNummerException("Personnummer format not handled: " + pnr);
+    }
+
+    private String getCenturyFromYearAndSeparator(String personnummer) {
+        final Calendar now = Calendar.getInstance();
+        final int currentYear = now.getWeekYear();
+        final boolean personnummerContainsCentury = personnummer.matches("[0-9]{8}[-+]?[0-9]{4}");
+        final int yearStartIndex = personnummerContainsCentury ? 2 : 0;
+        final int yearFromPersonnummer = Integer.parseInt(personnummer.substring(yearStartIndex, yearStartIndex + 2));
+        final int dividerToRemoveNonCenturyYear = 100;
+        final int century = (currentYear - yearFromPersonnummer) / dividerToRemoveNonCenturyYear;
+        if (personnummer.contains("+")) {
+            return String.valueOf(century - 1);
+        }
+        return String.valueOf(century);
+    }
+
+//    public boolean isSamordningsNummer() {
+//        final String normalizedPnr = getNormalizedPnrIfPossible(null);
+//        if (normalizedPnr == null) {
+//            return false;
+//        }
+//        return SamordningsnummerValidator.isSamordningsNummer(normalizedPnr);
+//    }
+
+    public String getPersonnummerWithoutDash() {
+        if (pnr == null) {
+            return null;
+        }
+        return pnr.replace("-", "");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Personnummer that = (Personnummer) o;
+        return Objects.equal(getNormalizedPnrIfPossible(pnr), that.getNormalizedPnrIfPossible(that.pnr));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getNormalizedPnrIfPossible(pnr));
+    }
+
+    public static String getPnrHashSafe(Personnummer personnummer) {
+        return personnummer == null ? HashUtility.hash(null) : personnummer.getPnrHash();
+    }
+
+    public static Personnummer empty() {
+        return new Personnummer(null);
+    }
+
+}
---
+++ contract/src/test/java/se/inera/intyg/schemas/contract/PersonnummerTest.java
@@ -0,0 +1,395 @@
+/*
+ * Copyright (C) 2017 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.intyg.schemas.contract;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import se.inera.intyg.schemas.contract.util.HashUtility;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PersonnummerTest {
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        ClassLoader.getSystemClassLoader().setClassAssertionStatus("se.inera.intyg.schemas.contract.Personnummer", false);
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        ClassLoader.getSystemClassLoader().setClassAssertionStatus("se.inera.intyg.schemas.contract.Personnummer", true);
+    }
+
+    @Test
+    public void testGetPersonnummer() throws Exception {
+        //Given
+        final String pnr = "190104063698";
+
+        //When
+        final Personnummer personnummer = new Personnummer(pnr);
+
+        //Then
+        assertEquals(pnr, personnummer.getPersonnummer());
+    }
+
+    @Test
+    public void testToStringShouldReturnTheHashedValue() throws Exception {
+        final List<String> pnrs = Arrays.asList("000000-0000", "00000000-0000", "000000000000", "0000000000", null, "");
+        for (String pnr : pnrs) {
+            try {
+                assertEquals(new Personnummer(pnr).getPnrHash(), new Personnummer(pnr).toString());
+            } catch (AssertionError ae) {
+                //I don't know how to disable assertions in code when running test via maven, hence let this assertion error pass
+            }
+        }
+    }
+
+    @Test
+    public void testGetPnrHash() throws Exception {
+        assertEquals("1424d7d0b8d4afd3c1ab1068a5a54d2d6d05d5b07e16effe36c176bd14b53c1c", new Personnummer("920926-2386").getPnrHash());
+        assertEquals("1424d7d0b8d4afd3c1ab1068a5a54d2d6d05d5b07e16effe36c176bd14b53c1c", new Personnummer("199209262386").getPnrHash());
+        assertEquals(HashUtility.EMPTY, new Personnummer(null).getPnrHash());
+        assertEquals(HashUtility.EMPTY, new Personnummer("").getPnrHash());
+        assertEquals(HashUtility.EMPTY, Personnummer.empty().getPnrHash());
+    }
+
+//    @Test
+//    public void testIsSamordningsNummer() throws Exception {
+//        assertFalse(SamordningsnummerValidator.isSamordningsNummer(new Personnummer("000000-0000")));
+//        assertTrue(SamordningsnummerValidator.isSamordningsNummer(new Personnummer("999999-9999")));
+//        assertFalse(SamordningsnummerValidator.isSamordningsNummer(new Personnummer("0000000000")));
+//        assertTrue(SamordningsnummerValidator.isSamordningsNummer(new Personnummer("9999999999")));
+//        assertFalse(SamordningsnummerValidator.isSamordningsNummer(new Personnummer("000000000000")));
+//        assertTrue(SamordningsnummerValidator.isSamordningsNummer(new Personnummer("199999999999")));
+//        assertFalse(SamordningsnummerValidator.isSamordningsNummer(new Personnummer("99999999999912345")));
+//    }
+
+    @Test
+    public void testGetPersonnummerWithoutDashRemovesDash() throws Exception {
+        //Given
+        final Personnummer personnummer = new Personnummer("000000-0000");
+
+        //When
+        final String personnummerWithoutDash = personnummer.getPersonnummerWithoutDash();
+
+        //Then
+        assertEquals("0000000000", personnummerWithoutDash);
+    }
+
+    @Test
+    public void testGetPersonnummerWithoutDashWillKeepPnrWithoutDashTheSame() throws Exception {
+        //Given
+        final String pnr = "0000000000";
+        final Personnummer personnummer = new Personnummer(pnr);
+
+        //When
+        final String personnummerWithoutDash = personnummer.getPersonnummerWithoutDash();
+
+        //Then
+        assertEquals(pnr, personnummerWithoutDash);
+    }
+
+    @Test
+    public void testTwoDifferentPnrsNotEquals() throws Exception {
+        //Given
+        final Personnummer personnummer0 = new Personnummer("000000-0000");
+        final Personnummer personnummer9 = new Personnummer("999999-9999");
+
+        //Then
+        assertFalse(personnummer0.equals(personnummer9));
+        assertFalse(personnummer9.equals(personnummer0));
+    }
+
+    @Test
+    public void testTwoIdenticalPnrsEquals() throws Exception {
+        //Given
+        final Personnummer personnummer0 = new Personnummer("000000-0000");
+        final Personnummer personnummer1 = new Personnummer("000000-0000");
+
+        //Then
+        assertTrue(personnummer0.equals(personnummer1));
+        assertTrue(personnummer1.equals(personnummer0));
+    }
+
+    @Test
+    public void testPnrWithOrWithoutDashEquals() throws Exception {
+        //Given
+        final Personnummer personnummer0 = new Personnummer("920926-2386");
+        final Personnummer personnummer1 = new Personnummer("9209262386");
+
+        //Then
+        assertTrue(personnummer0.equals(personnummer1));
+        assertTrue(personnummer1.equals(personnummer0));
+    }
+
+    @Test
+    public void testHashCode() throws Exception {
+        //Given
+        final Personnummer personnummer0 = new Personnummer("000000-0000");
+        final Personnummer personnummer9 = new Personnummer("999999-9999");
+
+        //Then
+        assertTrue(personnummer0.hashCode() != personnummer9.hashCode());
+        assertEquals(personnummer0.hashCode(), personnummer0.hashCode());
+    }
+
+    @Test
+    public void testGetPnrHashSafe() throws Exception {
+        assertEquals(HashUtility.EMPTY, Personnummer.getPnrHashSafe(null));
+        assertEquals(HashUtility.EMPTY, Personnummer.getPnrHashSafe(Personnummer.empty()));
+        assertEquals(HashUtility.EMPTY, Personnummer.getPnrHashSafe(new Personnummer(null)));
+        assertEquals(HashUtility.EMPTY, Personnummer.getPnrHashSafe(new Personnummer("")));
+
+        final Personnummer personnummer = new Personnummer("920926-2386");
+        assertEquals("1424d7d0b8d4afd3c1ab1068a5a54d2d6d05d5b07e16effe36c176bd14b53c1c", Personnummer.getPnrHashSafe(personnummer));
+        assertEquals(personnummer.getPnrHash(), Personnummer.getPnrHashSafe(personnummer));
+    }
+
+    @Test
+    public void testEmptyPersonnummerCanHandleAllCalls() throws Exception {
+        //Given
+        final Personnummer empty = Personnummer.empty();
+
+        //Then
+        assertEquals(HashUtility.EMPTY, empty.getPnrHash());
+        assertEquals(null, empty.getPersonnummer());
+        assertEquals(null, empty.getPersonnummerWithoutDash());
+        assertEquals(false, empty.equals(null));
+        assertEquals(31, empty.hashCode());
+    //    assertEquals(false, SamordningsnummerValidator.isSamordningsNummer(empty    ));
+
+        try {
+            assertEquals(HashUtility.EMPTY, empty.toString());
+        } catch (AssertionError ae) {
+            //I don't know how to disable assertions in code when running test via maven, hence let this assertion error pass
+        }
+
+        boolean exceptionThrownWhenNormalizing = false;
+        try {
+            empty.getNormalizedPnr();
+        } catch (InvalidPersonNummerException e) {
+            exceptionThrownWhenNormalizing = true;
+        }
+        assertTrue(exceptionThrownWhenNormalizing);
+
+        //Fail this test if a new method is added to Personnummer to make sure a call to it is added here
+        assertEquals(7, countNonPrivateMethodsInClass(Personnummer.class));
+
+    }
+
+    private int countNonPrivateMethodsInClass(Class<Personnummer> personnummerClass) {
+        final Method[] declaredMethods = personnummerClass.getDeclaredMethods();
+        int counter = 0;
+        for (Method declaredMethod : declaredMethods) {
+            int modifierBits = declaredMethod.getModifiers();
+            if (!Modifier.isPrivate(modifierBits) && !Modifier.isStatic(modifierBits)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    @Test
+    public void testGetNormalizedPnr() throws Exception {
+        //Given
+        final String pnr = "196001011111";
+        final String expected = "196001011111";
+
+        //When
+        final String normalizedPnr = new Personnummer(pnr).getNormalizedPnr();
+
+        //Then
+        assertEquals(expected, normalizedPnr);
+    }
+
+    @Test
+    public void testGetNormalizedPnr2() throws Exception {
+        //Given
+        final String pnr = "19600101-1111";
+        final String expected = "196001011111";
+
+        //When
+        final String normalizedPnr = new Personnummer(pnr).getNormalizedPnr();
+
+        //Then
+        assertEquals(expected, normalizedPnr);
+    }
+
+    @Test
+    public void testGetNormalizedPnr3() throws Exception {
+        //Given
+        final String pnr = "600101-1111";
+        final String expected = "196001011111";
+
+        //When
+        final String normalizedPnr = new Personnummer(pnr).getNormalizedPnr();
+
+        //Then
+        assertEquals(expected, normalizedPnr);
+    }
+
+    @Test
+    public void testGetNormalizedPnr4() throws Exception {
+        //Given
+        final String pnr = "600101+1111";
+        final String expected = "186001011111";
+
+        //When
+        final String normalizedPnr = new Personnummer(pnr).getNormalizedPnr();
+
+        //Then
+        assertEquals(expected, normalizedPnr);
+    }
+
+    @Test
+    public void testGetNormalizedPnr5() throws Exception {
+        //Given
+        final String pnr = "190101010101";
+        final String expected = "190101010101";
+
+        //When
+        final String normalizedPnr = new Personnummer(pnr).getNormalizedPnr();
+
+        //Then
+        assertEquals(expected, normalizedPnr);
+    }
+
+    @Test
+    public void testCreateValidatedPersonnummerWithDash() throws Exception {
+        parameterizedCreateValidatedPersonnummerWithDashTest(null, null);
+        parameterizedCreateValidatedPersonnummerWithDashTest("", null);
+        parameterizedCreateValidatedPersonnummerWithDashTest("19121212-1212", "19121212-1212");
+        parameterizedCreateValidatedPersonnummerWithDashTest("191212121212", "19121212-1212");
+        parameterizedCreateValidatedPersonnummerWithDashTest("1212121212", "20121212-1212");
+        parameterizedCreateValidatedPersonnummerWithDashTest("21121212-1212", null);
+        parameterizedCreateValidatedPersonnummerWithDashTest("20121212-1212", "20121212-1212");
+        parameterizedCreateValidatedPersonnummerWithDashTest("201212121212", "20121212-1212");
+        parameterizedCreateValidatedPersonnummerWithDashTest("121212+1212", "19121212-1212");
+        parameterizedCreateValidatedPersonnummerWithDashTest("121212+112", null);
+        parameterizedCreateValidatedPersonnummerWithDashTest("21212-1212", null);
+    }
+
+    private void parameterizedCreateValidatedPersonnummerWithDashTest(String input, String expected) {
+        Optional<Personnummer> pnr = Personnummer.createValidatedPersonnummerWithDash(input);
+        if(expected == null) {
+            assertFalse(String.format("Returned something when it should not have, %s", input), pnr.isPresent());
+        } else {
+            assertTrue(String.format("Failed to return something when it should have, %s", input), pnr.isPresent());
+            if(pnr.isPresent()) {
+                assertEquals(expected, pnr.get().getPersonnummer());
+            }
+        }
+    }
+
+    @Test
+    public void testSerializeDeserializePersonnummerNull() throws Exception {
+        //Given
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final Personnummer originalPnr = new Personnummer(null);
+
+        //When
+        final String json = objectMapper.writeValueAsString(originalPnr);
+
+        //Then
+        assertEquals("null", json);
+
+        //When
+        final Personnummer personnummer = objectMapper.readValue(json, Personnummer.class);
+
+        //Then
+        assertEquals(originalPnr.getPersonnummer(), personnummer.getPersonnummer());
+    }
+
+    @Test
+    public void testSerializeDeserializePersonnummerEmpty() throws Exception {
+        //Given
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final Personnummer originalPnr = new Personnummer("");
+
+        //When
+        final String json = objectMapper.writeValueAsString(originalPnr);
+
+        //Then
+        assertEquals("\"\"", json);
+
+        //When
+        final Personnummer personnummer = objectMapper.readValue(json, Personnummer.class);
+
+        //Then
+        assertEquals(originalPnr.getPersonnummer(), personnummer.getPersonnummer());
+    }
+
+    @Test
+    public void testSerializePersonnummer() throws Exception {
+        //Given
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final Personnummer value = new Personnummer("1234");
+
+        //When
+        final String json = objectMapper.writeValueAsString(value);
+
+        //Then
+        assertEquals("\"1234\"", json);
+    }
+
+    @Test
+    public void testDeserializePersonnummer() throws Exception {
+        //Given
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        //When
+        final Personnummer personnummer = objectMapper.readValue("\"1234\"", Personnummer.class);
+
+        //Then
+        assertEquals("1234", personnummer.getPersonnummer());
+    }
+//
+//    @Test
+//    public void testSerializeDeserializePersonnummerAsPartOfComplexType() throws Exception {
+//        //Given
+//        final ObjectMapper objectMapper = new ObjectMapper();
+//        final Personnummer originalPnr = new Personnummer("191212121212");
+//        final CertificateHolder complexType = new CertificateHolder();
+//        complexType.setCivicRegistrationNumber(originalPnr);
+//        complexType.setAdditionalInfo("test text");
+//
+//        //When
+//        final String json = objectMapper.writeValueAsString(complexType);
+//
+//        //Then
+//        assertTrue(json.contains("\"civicRegistrationNumber\":\"191212121212\""));
+//
+//        //When
+//        final CertificateHolder patient = objectMapper.readValue(json, CertificateHolder.class);
+//
+//        //Then
+//        assertEquals(originalPnr.getPersonnummer(), patient.getCivicRegistrationNumber().getPersonnummer());
+//    }
+
+}
