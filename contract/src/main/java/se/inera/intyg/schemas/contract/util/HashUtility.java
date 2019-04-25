36c03929b751dd462b941998db082da5c132afb9
Peter Larsson <peter.larsson@callistaenterprise.se>
2019-04-25 13:08:00 +0200

    INTYGFV-10875: hash function is now thread safe

--- contract/src/main/java/se/inera/intyg/schemas/contract/util/HashUtility.java
+++ contract/src/main/java/se/inera/intyg/schemas/contract/util/HashUtility.java
@@ -18,54 +18,25 @@
  */
 package se.inera.intyg.schemas.contract.util;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Strings;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
 import com.google.common.io.BaseEncoding;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public final class HashUtility {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(HashUtility.class);
-
-    private static final String DIGEST = "SHA-256";
-    private static final MessageDigest MSG_DIGEST;
-
     public static final String EMPTY = "EMPTY";
-    private static final String NO_HASH_VALUE = "NO-HASH-VALUE";
+
+    static HashFunction hf = Hashing.sha256();
 
     private HashUtility() {
     }
 
-    static {
-        MessageDigest tmp = null;
-        try {
-            tmp = MessageDigest.getInstance(DIGEST);
-        } catch (NoSuchAlgorithmException e) {
-            LOGGER.error("MessageDigest instantiation failed", e);
-        }
-        MSG_DIGEST = tmp;
-    }
-
-    public static String hash(String payload) {
+    public static String hash(final String payload) {
         if (Strings.isNullOrEmpty(payload)) {
             return EMPTY;
         }
-
-        if (MSG_DIGEST == null) {
-            LOGGER.error("Hashing not working due to MessageDigest not being instantiated");
-            return NO_HASH_VALUE;
-        }
-
-        try {
-            MSG_DIGEST.update(payload.getBytes("UTF-8"));
-            byte[] digest = MSG_DIGEST.digest();
-            return new String(BaseEncoding.base16().lowerCase().encode(digest));
-        } catch (UnsupportedEncodingException e) {
-            throw new IllegalStateException(e);
-        }
+        final byte[] digest = hf.hashString(payload, Charsets.UTF_8).asBytes();
+        return BaseEncoding.base16().lowerCase().encode(digest);
     }
 }
