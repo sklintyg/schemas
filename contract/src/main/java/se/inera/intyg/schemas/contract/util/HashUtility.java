735a892a9832b4eac9a58546f4a3f7d91c1c4fdf
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2026-02-11 12:20:16 +0100

    K1J-1847: Remove usage of deprecated Charsets and added final to hf

--- contract/src/main/java/se/inera/intyg/schemas/contract/util/HashUtility.java
+++ contract/src/main/java/se/inera/intyg/schemas/contract/util/HashUtility.java
@@ -18,16 +18,16 @@
  */
 package se.inera.intyg.schemas.contract.util;
 
-import com.google.common.base.Charsets;
 import com.google.common.base.Strings;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 import com.google.common.io.BaseEncoding;
+import java.nio.charset.StandardCharsets;
 
 public final class HashUtility {
     public static final String EMPTY = "EMPTY";
 
-    private static HashFunction hf = Hashing.sha256();
+    private static final HashFunction hf = Hashing.sha256();
 
     private HashUtility() {
     }
@@ -36,7 +36,7 @@ public final class HashUtility {
         if (Strings.isNullOrEmpty(payload)) {
             return EMPTY;
         }
-        final byte[] digest = hf.hashString(payload, Charsets.UTF_8).asBytes();
+        final byte[] digest = hf.hashString(payload, StandardCharsets.UTF_8).asBytes();
         return BaseEncoding.base16().lowerCase().encode(digest);
     }
 }
